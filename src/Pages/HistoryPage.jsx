9a2561523a6f41953737185be76342a941d01b7b
Zimhiii <hijazmih@gmail.com>
2024-10-03 19:46:29 +0700

    button ini history page

--- src/Pages/HistoryPage.jsx
+++ src/Pages/HistoryPage.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function HistoryPage() {
   return (
     <div>
-      <div className="flex font-montserrat text-[10px]">
-        <div className="py-[9px] px-[39px] bg-[#BB8360]">History</div>
-        <div className="py-[9px] px-[39px] ring-1 ring-[#BB8360]">
-          Keranjang
+      <div className=" relative flex font-montserrat text-[10px] justify-center py-[25px]">
+        <div className="flex bg-[#BB8360] rounded-[10px]  ">
+          <div className=" py-[9px] px-[39px]  bg-[#BB8360] rounded-[10px] text-white">
+            History
+          </div>
+          <div className="py-[9px]  px-[39px] ring-1 ring-[#BB8360] bg-white rounded-[10px]">
+            Keranjang
+          </div>
         </div>
       </div>
     </div>
